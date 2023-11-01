7d51d372661286223be755b65e541db03a7e3def
Rafael Dias <raffa96dias@gmail.com>
2023-10-31 21:57:53 -0300

    ex06

---
+++ Exercícios/Exercício06/Exercício01/ContaBancaria.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício01
+{
+    internal class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+        }
+
+        public ContaBancaria(int numero, string nome, double saldo) : this(numero, nome)
+        {
+            Saldo = saldo;
+        }
+
+        public void depositar(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void sacar(double saque)
+        {
+            Saldo -= saque + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Numero}, Titular: {Nome}, Saldo R$: {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+
+    }
+}
---
+++ Exercícios/Exercício06/Exercício01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria dados;
+
+            Console.Write("Insira o número da conta: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Insira o nome do títular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Havará depósito inicial (s/n)?");
+            string dpInicial = Console.ReadLine();
+
+            if (dpInicial == "s" || dpInicial == "S") 
+            {
+                // Se a verificaçao retornar verdadeira executa os codigos abaixo, inserindo um valor inicial no construtor
+                Console.Write("Insira o valor de deposito inicial: ");
+                double dp = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                dados = new ContaBancaria(num, titular, dp); // Atribui valores para os argumentos do construtor incluindo a sobrecarga
+
+            }
+            else
+            {
+                // Caso não seja verdadeira, atribui valores apenas ao construtor com dois parâmetros, e inicia o saldo da classe com o valor inicial "0"
+                dados = new ContaBancaria(num, titular);
+            }
+
+            // Depois das verificações imprime-se os dados inseridos pelo usuário
+            Console.WriteLine($"\nDados da conta:\n{dados}\n");
+
+            // Solicida um valor para deposito, e executa o método depositar declarado na classe ContaBancaria
+            Console.Write("insira um valor para deposito: ");
+            double deposito = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            dados.depositar(deposito);
+            Console.WriteLine($"Dados da conta atualizados:\n{dados}\n");
+
+            // Solicita um valor para saque, e executa o método sacar declarado na classe ContaBancaria
+            Console.Write("Insira um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            dados.sacar(saque);
+            Console.WriteLine($"Dados da conta atualizados:\n{dados}\n");
+
+        }
+    }
+}
